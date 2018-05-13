71f92f2f4bef4f4c6113012fee133141f9a2c49f
Evgeniy Rychkov <rychkov.evgeniy@gmail.com>
2018-05-13 19:16:52 +0600

    Upgrade Laravel 5.5=>5.6

--- app/Services/PermissionService.php
+++ app/Services/PermissionService.php
@@ -19,17 +19,17 @@ class PermissionService
         return PermissionGroup::all();
     }
 
-//    public function getAccessTypesByPermissionId()
-//    {
-//        $permissions = Permission::get();
-//        $config = $this->getPermissionsGroupsFromConfig();
-//        $result = new Collection();
-//        foreach ($permissions as $permission) {
-//            $permissionConfig = $config[$permission->permissionGroup->name]['permissions'][$permission->name];
-//            $result->put($permission->id, empty($permissionConfig['access_types']) ? new Collection([]) : new Collection($permissionConfig['access_types']));
-//        }
-//        return $result;
-//    }
+    public function getAccessTypesByPermissionId()
+    {
+        $permissions = Permission::get();
+        $config = $this->getPermissionsGroupsFromConfig();
+        $result = new Collection();
+        foreach ($permissions as $permission) {
+            $permissionConfig = $config[$permission->permissionGroup->name]['permissions'][$permission->name];
+            $result->put($permission->id, empty($permissionConfig['access_types']) ? new Collection([]) : new Collection($permissionConfig['access_types']));
+        }
+        return $result;
+    }
 
 //    public function getAccessTypesByPermissionId($permissionId)
 //    {

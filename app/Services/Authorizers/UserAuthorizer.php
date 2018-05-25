3c64702abdb703d60768ee82365251117c4bdbb6
Evgeniy Rychkov <rychkov.evgeniy@gmail.com>
2018-05-25 10:36:22 +0600

    Remove legacy code

--- app/Services/Authorizers/UserAuthorizer.php
+++ app/Services/Authorizers/UserAuthorizer.php
@@ -4,9 +4,6 @@ namespace App\Services\Authorizers;
 
 use App\Context\UserAuthorizeContext;
 use App\FactoryMethods\UserPermissionFactoryMethod;
-use App\Handlers\Permissions\User\AUserPermissionHandler;
-use App\PermissionGroup;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class UserAuthorizer extends AAuthorizer
 {

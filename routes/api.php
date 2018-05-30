789736bec9993c04ffe4b2d971bdbf9d8ab01b11
Andrey Vorobyev <a.vorobyev@tree-soft.com>
2018-05-30 20:40:03 +0600

    Add prefix for oauth token retriever

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function () {
-    Auth::routes();
-
-
     Route::resource('templates', 'TemplatesController');
     Route::resource('labels', 'LabelsController');
     Route::apiResource('types', 'TypesController', ['only' => ['index']]);
